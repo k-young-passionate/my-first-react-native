47ac8563ff263f1d78d92b81b6a5e0580d1910bc
Keonyoung Shim <kyshim9004@gmail.com>
2020-11-27 13:08:10 +0900

    [storybook] remove unnecessary lines

--- storybook/stories/Drawer/Drawer.stories.tsx
+++ storybook/stories/Drawer/Drawer.stories.tsx
@@ -1,9 +1,6 @@
-import { Text, View, StyleSheet, Button, SafeAreaView } from 'react-native';
-
-import React, { useState } from 'react';
+import React from 'react';
 import { storiesOf } from '@storybook/react-native';
 
-import CenterView from '../CenterView';
 import CustomDrawer from '.';
 
 storiesOf('Custom Drawer', module)
