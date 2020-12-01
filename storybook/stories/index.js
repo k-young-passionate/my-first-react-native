e608c15686e70440a3e46048dd866a20e34e531e
Keonyoung Shim <kyshim9004@gmail.com>
2020-12-01 14:12:09 +0900

    [storybook] update index.js

--- storybook/stories/index.js
+++ storybook/stories/index.js
@@ -3,3 +3,5 @@ import './Welcome/Welcome.stories';
 import './Axios/AxiosTest.stories';
 import './TextInput/TextInput.stories';
 import './Drawer/Drawer.stories';
+import './Popup/Popup.stories';
+import './ScreenMapper/ScreenMapper.stories'
